9f4b9b7fea103ea5d439f12cf7ea252371b76dde
Edson Marinho <edson.com@gmail.com>
2023-11-23 15:27:42 -0300

    Library ok

--- test/index.php
+++ test/index.php
@@ -1,10 +1,13 @@
 <?php
 
-require __DIR__ . '/lib_ext/autoload.php';
+require __DIR__ . '/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-$novoEmail = new Email;
+$novoEmail = new Email(2, "smtp-mail.outlook.com", "edson_mario@hotmail.com",
+    "teste123", "tls", "587", "edson_mario@hotmail.com",
+    "Equipe EdsonWeb");
+
 $novoEmail->sendEmail("Teste de e-mail", "<p>Esse é um e-mail de <b>teste</b>!</p>",
     "edson.com@gmail.com", "Edson Web", "edson_mario@hotmail.com",
     "Edson Mario");
